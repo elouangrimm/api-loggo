6455a5886583a7f49971c0f809cf9d7bf9795dcc
Svenlaa <mail@svenlaa.com>
2024-12-27 10:57:55 +0100

    Initial Commit ✨

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+    return c.html('Why are you here? Visit <a href="https://svenlaa.com">svenlaa.com</a> for the site!');
+});
+
+export default app;
