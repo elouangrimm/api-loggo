ca3f8953ef0db519c4f9f0f97d563361831a0839
Svenlaa <mail@svenlaa.com>
2024-12-27 11:04:21 +0100

    Responsiveness

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
-app.get('/', (c) => c.html('Why are you here? Visit <a href="https://svenlaa.com">svenlaa.com</a> for the site!'));
+app.get('/', (c) =>
+    c.html(
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>api.svenlaa.com</title>Why are you here? Visit <a href="https://svenlaa.com">svenlaa.com</a> for the site!'
+    )
+);
 
 export default app;
