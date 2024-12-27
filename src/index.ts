ded8923591c0775d2e3457b5a78aa6290a02f230
Svenlaa <mail@svenlaa.com>
2024-12-27 14:06:32 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from 'hono';
 import logiverse from './logiverse';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
-
+app.use(cors({ origin: '*' }));
 app.get('/', (c) =>
     c.html(
         '<meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>api.svenlaa.com</title>Why are you here? Visit <a href="https://svenlaa.com">svenlaa.com</a> for the site!'
