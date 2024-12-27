882170c8a4dd4b2af08db05c264a10c78429d0fe
Elouan Grimm <elouan.grimm@gmail.com>
2024-12-27 09:43:32 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = new Hono()
     .use(cors({ origin: '*' }))
     .get('/', (c) =>
         c.html(
-            '<meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>api.svenlaa.com</title>Why are you here? Visit <a href="https://svenlaa.com">svenlaa.com</a> for the site!'
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>api.svenlaa.com</title>Why are you here? Visit <a href="https://loggo.pages.dev">loggo.pages.dev</a> for the site!'
         )
     );
 
